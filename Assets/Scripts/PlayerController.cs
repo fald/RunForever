4a54f1a2c81f127b6f91a16638a5452bc65acd92
Firas <firasaldabbas+git@gmail.com>
2019-09-03 22:13:50 -0400

    Little dude runs and jumps; multi-jumps, too.
    Bug with multi jump though, jumps > 0 result in one more jump than expected!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed;
+    public float jumpForce;
+    public int numJumps;
+    public int maxJumps;
+
+    public bool grounded;
+    public LayerMask groundLayer;
+
+    // Collider as opposed to box collider for generalization
+    private Collider2D myCollider;
+
+    private Rigidbody2D myRigidBody; // ;)
+
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
+        numJumps = maxJumps;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        grounded = Physics2D.IsTouchingLayers(myCollider, groundLayer);
+        if (grounded)
+        {
+            numJumps = maxJumps;
+        }
+
+        myRigidBody.velocity = new Vector2(moveSpeed, myRigidBody.velocity.y);
+
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && (numJumps > 0))
+        {
+            numJumps--;
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+        }
     }
 }
