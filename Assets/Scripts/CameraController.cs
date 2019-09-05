c21c311145b1212a6fd83890e5efe90f81e9fd28
Firas <firasaldabbas+git@gmail.com>
2019-09-04 23:32:10 -0400

    Camera controller done
    Pretty sure it can be done way better though

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public PlayerController thePlayer;
+    private Vector3 lastPlayerPosition;
+    private float distanceToMove;
+
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerController>();
+        lastPlayerPosition = thePlayer.transform.position;
+    }
+
+    void FixedUpdate()
+    {   
+        // Surely you can just increase the y-position by player speed instead of this shit, 
+        // while keeping y position constant? Or if y pos was to be updated w/ player, just
+        // attach camera as child of Player
+        distanceToMove = thePlayer.transform.position.x - lastPlayerPosition.x;
+        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+        lastPlayerPosition = thePlayer.transform.position;
+    }
+}
