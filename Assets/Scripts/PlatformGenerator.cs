43009b8343a2a760008b7448a6cde16eaad1025c
Firas <firasaldabbas+git@gmail.com>
2019-09-06 00:00:18 -0400

    Infinite generation of platforms
    Simplistic as it is.

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+    public Transform generationPoint;
+    public GameObject platform;
+    public float distanceBetween;
+    private float platformWidth;
+
+    void Start()
+    {
+        platformWidth = platform.GetComponent<BoxCollider2D>().size.x;    
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.position.x < generationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+            Instantiate(platform, transform.position, transform.rotation);
+        }
+    }
+}
