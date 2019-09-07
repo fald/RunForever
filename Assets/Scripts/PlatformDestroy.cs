7803d1952ba44c506b4f236c7411a6f63e398a31
Firas <firasaldabbas+git@gmail.com>
2019-09-06 23:10:54 -0400

    Platform destruction

---
+++ Assets/Scripts/PlatformDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestroy : MonoBehaviour
+{
+    public GameObject platformDestructionPoint;
+
+    void Start()
+    {
+        platformDestructionPoint = GameObject.Find("Platform Destruction Point");
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.position.x < platformDestructionPoint.transform.position.x)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
