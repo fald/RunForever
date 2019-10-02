65442bcc22fef3b746d4bd7b86456723931af6bf
Firas <firasaldabbas+git@gmail.com>
2019-10-02 19:08:14 -0400

    pause screen + button

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// TODO: Set central quite/restart, this is dumb and wet.
+public class PauseMenu : MonoBehaviour
+{
+    public string mainSceneName;
+    public GameManager gm;
+    public GameObject pauseMenu;
+
+    public void restart()
+    {
+        ResumeGame();
+        gm.Reset();
+    }
+
+    public void quitToMenu()
+    {
+        ResumeGame();
+        SceneManager.LoadScene(mainSceneName);
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
