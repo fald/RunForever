db057e0b34589bf060bda4cc44d22403cf548470
Firas <firasaldabbas+git@gmail.com>
2019-10-04 13:53:49 -0400

    powerups now work

--- Assets/Scripts/PowerUpManager.cs
+++ Assets/Scripts/PowerUpManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // TODO: Better powerup management lol
+// TODO: Find out whats up with find object of type not working
+// TODO: Indicator of buff + duration
 
 public class PowerUpManager : MonoBehaviour
 {
@@ -11,13 +13,14 @@ public class PowerUpManager : MonoBehaviour
     private float duration;
     private bool isActive;
 
-    private ScoreManager scoreMgr;
-    private PlayerController playerCtrl;
+    public ScoreManager scoreMgr;
+    public PlayerController playerCtrl;
 
     public void Start()
     {
-        scoreMgr = FindObjectOfType<ScoreManager>();
-        playerCtrl = FindObjectOfType<PlayerController>();
+        // some trouble with finding these for whatever reason
+        //scoreMgr = FindObjectOfType<ScoreManager>();
+        //playerCtrl = FindObjectOfType<PlayerController>();
     }
 
     public void Awake()
@@ -31,12 +34,16 @@ public class PowerUpManager : MonoBehaviour
         invulnerable = invul;
         duration = timeSet;
         isActive = true;
-        scoreMgr.pointsPerSecond = points ? scoreMgr.pointsPerSecond *= 2 : 1;
+        // scoreMgr.pointsPerSecond = points ? scoreMgr.pointsPerSecond *= 2 : 1;
+        scoreMgr.pointsPerSecond = points ? 2 : 1;
         playerCtrl.isInvulnerable = invul ? true : false;
     }
 
     public void DeactivatePowers()
     {
+        doublePoints = false;
+        invulnerable = false;
+        duration = 0;
         scoreMgr.pointsPerSecond = 1;
         playerCtrl.isInvulnerable = false;
         isActive = false;
